ee2d4f80dcbffeb288a4d29b0ea1351b1f597135
Janne <Janne@JANNE-PC>
2017-12-11 17:15:48 +0200

    Day 11

--- AdventOfCode17/StartUp.cs
+++ AdventOfCode17/StartUp.cs
@@ -11,7 +11,7 @@ namespace AdventOfCode17
     {
         static void Main()
         {
-            new day10();
+            new day11();
             Console.ReadKey();
         }
     }
---
+++ AdventOfCode17/day11.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode17
+{
+    class day11
+    {
+        public day11()
+        {
+            string input = File.ReadAllText(@"day11input.txt");
+            var directions = input.Split(',');
+
+            int xdir = 0;
+            int ydir = 0;
+
+            int maxdist = 0;
+
+            foreach (string dir in directions)
+            {
+                switch (dir)
+                {
+                    case "n":
+                        ydir += 2;
+                        break;
+                    case "ne":
+                        xdir += 1;
+                        ydir += 1;
+                        break;
+                    case "se":
+                        xdir += 1;
+                        ydir -= 1;
+                        break;
+                    case "s":
+                        ydir -= 2;
+                        break;
+                    case "sw":
+                        xdir -= 1;
+                        ydir -= 1;
+                        break;
+                    case "nw":
+                        xdir -= 1;
+                        ydir += 1;
+                        break;
+                    default:
+                        throw new ArgumentException();
+                }
+
+                int dist = distance(xdir, ydir);
+                if (dist > maxdist) maxdist = dist;
+            }
+
+            Console.WriteLine(distance(xdir, ydir));
+            Console.WriteLine(maxdist);
+
+        }
+
+        public int distance(int x, int y)
+        {
+            //Diagonal steps so changes both X and Y distance
+            int moveXY = Math.Abs(x);
+
+            //Once diagonal moves "run out" move the rest of the way straight up/down
+            int moveY = (Math.Abs(y) - moveXY) / 2;
+
+            return moveXY + moveY;
+        }
+    }
+}
